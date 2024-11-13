50e7c3225f201661f0a6cb599382cc86e60b28d3
martian58 <fuadelizade6@gmail.com>
2024-11-13 10:29:42 +0400

    Update admin resgister

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Customer
 
-# Register your models here.
 
-
-admin.site.register(Customer)
+@admin.register(Customer)
+class GroupMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'phone', 'address')
